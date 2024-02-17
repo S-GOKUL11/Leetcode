6c61a595f13a94e3a1f5386cb6bcf5714134bbe4
GOKUL S <109498355+S-GOKUL11@users.noreply.github.com>
2024-02-17 20:31:02 +0530

    Create 13. Roman to Integer.c
    https://leetcode.com/problems/roman-to-integer/submissions/1177909981/

---
+++ C/13. Roman to Integer.c
@@ -0,0 +1,50 @@
+int romanToInt(char* s) {
+    int Roman = 0;
+    for (int i = 0; i < strlen(s); i++) {
+        switch (s[i]) {
+        case 'I':
+            if (s[i + 1] == 'V' || s[i + 1] == 'X') {
+                Roman -= 1;
+                break;
+            } else {
+                Roman += 1;
+                break;
+            }
+        case 'V': {
+            Roman += 5;
+            break;
+        }
+        case 'X': {
+            if (s[i + 1] == 'L' || s[i + 1] == 'C') {
+                Roman -= 10;
+                break;
+            } else {
+                Roman += 10;
+                break;
+            }
+        }
+        case 'L': {
+            Roman += 50;
+            break;
+        }
+        case 'C': {
+            if (s[i + 1] == 'D' || s[i + 1] == 'M') {
+                Roman -= 100;
+                break;
+            } else {
+                Roman += 100;
+                break;
+            }
+        }
+        case 'D': {
+            Roman += 500;
+            break;
+        }
+        case 'M': {
+            Roman += 1000;
+            break;
+        }
+        }
+    }
+    return Roman;
+}
